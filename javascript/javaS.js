38d49effcac228b54e302bef6229b6a1ddffe48d
Kevin Kriegel <kevinkriegel07@gmail.com>
2020-02-10 11:14:02 +0100

    Grille prete

--- javascript/javaS.js
+++ javascript/javaS.js
@@ -24,19 +24,42 @@ let grille=[
     [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
 ];
 
-function initGrille()
-{
 
-var laGrille =document.querySelector(".grille");
-laGrille.innerHTML="";
 
-let i = 1;
+
+function initGrille(){
+/* vider la grille */
+let i= 1;
 let y = 1;
 
+var laGrille =document.querySelector("#grille");
+
+laGrille.innerHTML="";
+/* /* dimension dynamique de la grille */
 laGrille.style.gridTemplateRows = "repeat(22, 40px)";
 laGrille.style.gridTemplateColumns = "repeat(19, 40px)";
 
-}
+for(let ligne in grille){
+    for(let col in grille [ligne]){
+        let monelement=document.createElement("div");
+
+        if ((grille[ligne][col]) ==(0)){
+            monelement.classList.add("mur");
+        }
+
+        else    if((grille[ligne][col])==(1)){
+            monelement.classList.add("sol");
+        }
+
+        else    if((grille[ligne][col])==(2)){
+            monelement.classList.add("bonbon");
+        }
+        monelement.style.gridColumn=(+col)+1;
+        monelement.style.gridRow=(+ligne)+1;
+        laGrille.appendChild(monelement);
+}}}
+
+
 
 initGrille();
 
